e1783d35fbfb96caaaed1d4a0c856c8c366340d5
Alejandro <posdata.5@hotmail.com>
2019-01-09 19:25:48 -0600

    Able to send emails successfully now

--- autoEmail.py
+++ autoEmail.py
@@ -1,8 +1,33 @@
 import os
 import sys
-import credentials
+from credentials import originEmail, password, destinationEmail
+import smtplib
 
-#sys.path.insert(0, 'D:\\Python\\autoResponder\\Modules')
-print (sys.path)
+#Outlook port
+port = 587
 
-'D:\\Python\\autoResponder', 
+#Depending in encryption, choose what kind of object to create
+
+#SSL standard
+#smtpObj = smtplib.SMTP_SSL('smtp-mail.outlook.com', 465)
+
+#SMTP server domain name - TLS
+smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
+#print (type(smtpObj))
+
+#Check for errors in object
+#print (smtpObj.ehlo())
+
+#Set up encrypted SMTP connection
+smtpObj.starttls()
+
+#Logging in
+#print (smtpObj.login(originEmail, password))
+smtpObj.login(originEmail, password)
+
+#Test of sending an originEmail
+smtpObj.sendmail(originEmail, destinationEmail,
+'Subject: So long.\nDear Alice, so long and thanks for all the fish. Sincerely, Bob')
+
+#Logging off
+smtpObj.quit()
