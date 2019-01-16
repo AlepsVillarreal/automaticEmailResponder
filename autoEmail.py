7117562ab31fa1632c0b4814124ed4e4208d5273
Alejandro Villarreal <androreal.av@gmail.com>
2019-01-16 11:35:25 -0600

    Added a check to see if the emamil comes from pusher - Added a dict to get current date in string value to pass as parameter. Couldn't get it to work for the search criteria. Goal now is to make it fit, then iterate through all emails in that date, get all their bodies, check if it passes the condition

--- autoEmail.py
+++ autoEmail.py
@@ -5,76 +5,95 @@ import imapclient
 import pprint
 from credentials import originEmail, password, destinationEmail
 import smtplib
+import datetime
 import html2text
 
-#Outlook port
-port = 587
+#Setting up dynamic variables for search criteria
+numericalMonthStringDict = {1:'Jan'}
 
-#Depending in encryption, choose what kind of object to create
+#Get current date
+now = datetime.datetime.now()
 
-#SSL standard
-#smtpObj = smtplib.SMTP_SSL('smtp-mail.outlook.com', 465)
+for key, value in numericalMonthStringDict.items():
+	if now.month == key:
+		month = value
 
-#SMTP server domain name - TLS
-smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
-#print (type(smtpObj))
+dynamicDate = '%d-%s-%d' %(now.day, month, now.year)
 
-#Check for errors in object
-#print (smtpObj.ehlo())
+searchParameter = "\'(ON \"%d-%s-%d\")\'" %(now.day, month, now.year)
 
-#Set up encrypted SMTP connection
-smtpObj.starttls()
+print ('searchParameter is ' + searchParameter)
 
-#Logging in
-#print (smtpObj.login(originEmail, password))
-smtpObj.login(originEmail, password)
-
-#Test of sending an originEmail
-#smtpObj.sendmail(originEmail, destinationEmail,
-#'Subject: So long.\nDear Alice, so long and thanks for all the fish. Sincerely, Bob')
-
-#Logging off
-smtpObj.quit()
 
 #Trying out IMAP to retrieve emails
 imapObj = imapclient.IMAPClient('imap-mail.outlook.com', ssl=True)
+
+#Selecting UIDs of emails - setting criteria
+#UIDs = imapObj.search('(ON "15-Jan-2019")')
 
 #logging in imap object
 imapObj.login(originEmail, password)
 
 #Selecting folder to read files from
-imapObj.select_folder('INBOX', readonly=True)
+imapObj.select_folder('INBOX/Pusher.py/ACDB1', readonly=True)
 
 #pprint.pprint(imapObj.list_folders())
 
-#Selecting UIDs of emails - setting criteria
-UIDs = imapObj.search('(ON "15-Jan-2019")')
+##Selecting UIDs of emails - setting criteria
+#UIDs = imapObj.search('(ON "15-Jan-2019")')
+#
+##Printing UIDs of emails
+#print (UIDs)
+#
+##Fetching emails
+#fetched = imapObj.fetch(UIDs,  ['BODY[]'])
+#
+###UIDs to use [5350, 5354, 5360, 5367, 5371, 5378]
+##[538, 540, 542, 544, 546, 548, 550, 552, 554, 556, 558]
+#message = pyzmail.PyzMessage.factory(fetched[556][b'BODY[]'])
+##
+#print (message.get_subject())
+#
+#print (message.get_addresses('from'))
+#
+#print (message.get_addresses('to'))
 
-#Printing UIDs of emails
-print (UIDs)
-
-#Fetching emails
-fetched = imapObj.fetch(UIDs,  ['BODY[]'])
 
 
-#UIDs to use [5350, 5354, 5360, 5367, 5371, 5378]
-message = pyzmail.PyzMessage.factory(fetched[5367][b'BODY[]'])
-
-print (message.get_subject())
-
-print (message.get_addresses('from'))
-
-print (message.get_addresses('to'))
-
-#print (message.text_part.get_payload().decode(message.text_part.charset))
-
-#print (message.html_part.get_payload().decode(message.text_part.charset))
-#print (message.html_part.get_payload().decode(message.html_part.charset))
-
-body = html2text.html2text(message.html_part.get_payload().decode(message.html_part.charset))
-
-print (body)
+##THIS PART READS AN INDIVIDUAL EMAIL BODY AND DETECTS WHEN RTN IS BESIDES 0
+#bodyOfPusherEmails = (message.text_part.get_payload().decode(message.text_part.charset))
+#
+#for word in bodyOfPusherEmails.split():
+#	#print ('word is: ' + word)
+#	if 'RTN:0' in word:
+#		print ('It went fine')
+#	else:
+#		#Send an email saying that it broke
+#		print ('It broke')
 
 imapObj.logout()
 
 
+
+##Sending email part
+#Outlook port
+#port = 587
+##SMTP server domain name - TLS
+#smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
+#
+##Check for errors in object
+##print (smtpObj.ehlo())
+#
+##Set up encrypted SMTP connection
+#smtpObj.starttls()
+#
+##Logging in
+##print (smtpObj.login(originEmail, password))
+#smtpObj.login(originEmail, password)
+#
+##Test of sending an originEmail
+##smtpObj.sendmail(originEmail, destinationEmail,
+##'Subject: So long.\nDear Alice, so long and thanks for all the fish. Sincerely, Bob')
+#
+##Logging off
+#smtpObj.quit()
