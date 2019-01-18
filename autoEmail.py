03e040c9efc2c44c0f8a2056f2c52c0e86b8043e
Alejandro Villarreal <androreal.av@gmail.com>
2019-01-17 21:55:09 -0600

    Added filter for a certain email and print the subject of the email when RTN is 1

--- autoEmail.py
+++ autoEmail.py
@@ -20,9 +20,9 @@ for key, value in numericalMonthStringDict.items():
 
 dynamicDate = '%d-%s-%d' %(now.day, month, now.year)
 
-searchParameter = "\'(ON \"%d-%s-%d\")\'" %(now.day, month, now.year)
+#searchParameter = "(ON \"%d-%s-%d\")" %(now.day, month, now.year)
 
-print ('searchParameter is ' + searchParameter)
+#print ('searchParameter is ' + searchParameter)
 
 
 #Trying out IMAP to retrieve emails
@@ -34,19 +34,39 @@ imapObj = imapclient.IMAPClient('imap-mail.outlook.com', ssl=True)
 #logging in imap object
 imapObj.login(originEmail, password)
 
-#Selecting folder to read files from
-imapObj.select_folder('INBOX/Pusher.py/ACDB1', readonly=True)
+### ACDB1 SET
+##Selecting folder to read files from
+#imapObj.select_folder('INBOX/Pusher.py/ACDB1', readonly=True)
+###Selecting UIDs of emails - setting criteria
+#UIDs = imapObj.search('(FROM dwopr@AUSVACDODB02.activant.com)') 
 
-#pprint.pprint(imapObj.list_folders())
+### DEV SET
+##Selecting folder to read files from
+imapObj.select_folder('INBOX/Pusher.py/DEV', readonly=True)
+###Selecting UIDs of emails - setting criteria
+UIDs = imapObj.search('(FROM dwopr@ausxeptdmo02.activant.com)') 
 
-##Selecting UIDs of emails - setting criteria
-#UIDs = imapObj.search('(ON "15-Jan-2019")')
-#
+
+
 ##Printing UIDs of emails
-#print (UIDs)
-#
+print (UIDs)
 ##Fetching emails
-#fetched = imapObj.fetch(UIDs,  ['BODY[]'])
+fetched = imapObj.fetch(UIDs,  ['BODY[]'])
+i = 0
+for element in UIDs:
+	message = pyzmail.PyzMessage.factory(fetched[element][b'BODY[]'])
+	bodyOfPusherEmails = (message.text_part.get_payload().decode(message.text_part.charset))
+	for word in bodyOfPusherEmails.split():
+		#print ('word is: ' + word)
+		if 'RTN:0' in word:
+			break
+		elif 'RTN:1' in word:
+			#Send an email saying that it broke	
+			i += 1
+			print ("%d - error in: "%i)
+			print(message.get_subject())
+
+
 #
 ###UIDs to use [5350, 5354, 5360, 5367, 5371, 5378]
 ##[538, 540, 542, 544, 546, 548, 550, 552, 554, 556, 558]
@@ -64,12 +84,12 @@ imapObj.select_folder('INBOX/Pusher.py/ACDB1', readonly=True)
 #bodyOfPusherEmails = (message.text_part.get_payload().decode(message.text_part.charset))
 #
 #for word in bodyOfPusherEmails.split():
-#	#print ('word is: ' + word)
-#	if 'RTN:0' in word:
-#		print ('It went fine')
-#	else:
-#		#Send an email saying that it broke
-#		print ('It broke')
+	#print ('word is: ' + word)
+	#if 'RTN:0' in word:
+	#	print ('It went fine')
+	#else:
+	#	#Send an email saying that it broke
+	#	print ('It broke')
 
 imapObj.logout()
 
